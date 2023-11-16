4d7d32b0c7ac4615db12d3e5b55230df9784f8eb
Talitha Firyal <talithafiryalgn@gmail.com>
2023-11-16 15:20:59 +0700

    pertemuan 16-11-2023

---
+++ resources/views/latian.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>John J. Burns Library(burnslibrary) - Official - Linktree</title>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@300;500;600;700&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;600;700&amp;display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+</html>
