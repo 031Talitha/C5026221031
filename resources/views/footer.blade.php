c1d51c260cd0b3308bd1aea3f810fe73f4ea634a
Talitha Firyal <talithafiryalgn@gmail.com>
2023-11-30 14:59:01 +0700

    Pertemuan 30 Nop 2023

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<br/>
+<br/>
+<hr/>
+<footer>
+    <p>&copy; Talitha Firyal, 2023</p>
+</footer>
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -31,7 +31,7 @@
 <br/>
 <hr/>
 <footer>
-    <p>&copy; <a href="https://www.mlaasngoding.com">www.malasngoding.com</a>. 2018 - 2019</p>
+    <p>&copy; <a href="https://www.malasngoding.com">www.malasngoding.com</a>. 2018 - 2019</p>
 </footer>
 </div>
 
